cbd55e9850d0da9c4c97c299a4a609946bc1788a
chaozhou <chaozhou@tencent.com>
2023-11-28 21:46:16 +0800

    fix

--- src/Miniprogram.php
+++ src/Miniprogram.php
@@ -2,29 +2,84 @@
 
 namespace Superzc\Miniprogram;
 
-use Illuminate\Config\Repository;
+// use Illuminate\Config\Repository;
+use Illuminate\Support\Facades\Http;
 
 class Miniprogram {
 
-    protected $config;
+    protected $appid;
+    protected $appsecret;
 
     /**
      * 构造方法
      */
-    public function __construct(Repository $config)
+    public function __construct($appid, $appsecret)
     {
-        $this->config = $config->get('miniprogram');
+        $this->appid = $appid;
+        $this->appsecret = $appsecret;
+    }
+
+    /**
+     * 获取接口调用凭据
+     * https://developers.weixin.qq.com/miniprogram/dev/OpenApiDoc/mp-access-token/getAccessToken.html
+     */
+    public function getAccessToken()
+    {
+        $response = Http::get("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" . $this->appid . "&secret=" . $this->appsecret);
+
+        return $this->processResponse($response);
+    }
+
+    /**
+     * 获取稳定版接口调用凭据
+     * https://developers.weixin.qq.com/miniprogram/dev/OpenApiDoc/mp-access-token/getStableAccessToken.html
+     */
+    public function getStableAccessToken($force_refresh = false)
+    {
+        $postData = [
+            'grant_type' => 'client_credential',
+            'appid' => $this->appid,
+            'secret' => $this->appsecret,
+            'force_refresh' => $force_refresh,
+        ];
+
+        $response = Http::post("https://api.weixin.qq.com/cgi-bin/stable_token", json_encode($postData));
+
+        return $this->processResponse($response);
     }
     
-    public function codeToSession()
+    /**
+     * 小程序登录
+     * https://developers.weixin.qq.com/miniprogram/dev/OpenApiDoc/user-login/code2Session.html
+     */
+    public function codeToSession($code)
+    {
+        $response = Http::get("https://api.weixin.qq.com/sns/jscode2session?appid=" . $this->appid . "&secret=" . $this->appsecret . "&js_code=" . $code . "&grant_type=authorization_code");
+
+        return $this->processResponse($response);
+    }
+
+    /**
+     * 处理响应
+     */
+    private function processResponse($response)
     {
         $data = [];
 
-        return [
-            'ret' => 0,
-            'msg' => 'success1',
-            'data' => $data,
-        ];
+        if ($response->successful()) {
+            $data = $response->json();
+        } elseif ($response->failed()) {
+            // 请求失败的处理逻辑
+            return false;
+        } elseif ($response->clientError()) {
+            // 客户端错误 4xx 的处理逻辑
+            return false;
+        } elseif ($response->serverError()) {
+            // 服务器错误 5xx 的处理逻辑
+            return false;
+        }
+
+        return $data;
     }
  
 }
